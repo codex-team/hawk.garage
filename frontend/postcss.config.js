de9a4792f26e5afb975199ccce585a6001c1dd8a
Nikita Melnikov <nikmel2803@gmail.com>
2019-01-04 14:38:56 +0300

    add postcss plugins and home page markup

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
+    'postcss-import': {},
+    'postcss-apply': {},
+    'postcss-custom-properties': {},
+    'postcss-custom-media': {},
+    'postcss-media-minmax': {},
+    'postcss-custom-selectors': {},
+    'postcss-nested-ancestors': {},
+    'postcss-nesting': {},
+    'postcss-nested': {},
+    'postcss-font-family-system-ui': {},
+    'autoprefixer': {}
   }
 }
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,145 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="yard-home">
+    <h1 class="yard-home__title">Hawk.so</h1>
+    <p class="yard-home__caption">
+      Fast and lightweight errors tracking service. Backend and Frontend events
+      monitoring.
+    </p>
+    <ul class="yard-home__links">
+      <li v-if="authed">
+        <a class="primary" href="/garage">
+          <!--{{ user.email|escape }}-->
+          <!--{{ svg('/public/svg/arrow-right.svg') }}-->
+          nikmel2803@gmail.com
+        </a>
+      </li>
+      <template v-else>
+        <li>
+          <a class="primary" href="/garage">Try for free</a>
+        </li>
+        <li>
+          <a href="/join">Sign up</a>
+        </li>
+        <li>
+          <a href="/login">Login</a>
+        </li>
+      </template>
+
+      <li>
+        <a href="/docs">Documentation</a>
+      </li>
+    </ul>
+    <figure class="yard-home__figure">
+      <!--{# <figcaption></figcaption> #}-->
+      <!--<a href="/garage">-->
+      <!--<img src="/static/images/index_page/list.png" alt="Backend and Frontend errors monitoring">-->
+      <!--</a>-->
+    </figure>
+
+    <div class="yard-home__footer">
+      Made with inspiration by
+      <a href="//ifmo.su">CodeX</a>.
+      <div class="fl_r">
+        Feel free to leave a Feedback to the
+        <a href="mailto:team@ifmo.su?subject=Hawk">team@ifmo.su</a>.
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: "home",
+  data() {
+    return {
+      authed: false
+    };
+  }
+};
+</script>
+
+<style lang="postcss">
+@import url("../stylesheets/variables.css");
+
+.yard-home {
+  width: 100%;
+  background: var(--yardBg);
+  color: var(--yardGrayText);
+  padding: 60px;
+  padding-bottom: 20px;
+  font-size: 20px;
+
+  @media (--mobile) {
+    padding: 15px;
+  }
+
+  &__title {
+    font-size: 2.6em;
+    color: #fff;
+  }
+
+  &__caption {
+    color: var(--yardGrayText);
+    line-height: 1.4em;
+  }
+
+  &__links {
+    margin-top: 1em;
+    font-weight: bold;
+
+    li {
+      display: inline-block;
+      margin-right: 1em;
+      margin-bottom: 0.6em;
+    }
+
+    a {
+      color: #fff;
+
+      &.primary {
+        color: var(--linkOnDark);
+      }
+
+      svg {
+        margin-left: 0.6em;
+      }
+    }
+  }
+
+  &__figure {
+    margin: 0 0 30px;
+    color: var(--yardGrayText);
+
+    img {
+      margin-top: 1em;
+      max-width: 100%;
+    }
+  }
+
+  &__footer {
+    border-top: 1px solid color(var(--yardBg) lightness(+5%));
+    font-size: 15px;
+    padding: 20px 0 0;
+    letter-spacing: 0.18px;
+
+    a {
+      color: var(--greenText);
+      border-bottom: 1px solid color(var(--greenText) alpha(60%));
+
+      &:hover {
+        color: #fff;
+        border-bottom-color: color(#fff alpha(60%));
+      }
+    }
+
+    .fl_r {
+      @media (--mobile) {
+        float: none;
+        margin-top: 8px;
+      }
+    }
   }
 }
-</script>
+</style>
