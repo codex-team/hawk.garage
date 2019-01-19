76f06cd1c9159f849761e77fa8b4df028ab84d76
Nikita Melnikov <nikmel2803@gmail.com>
2019-01-20 00:32:09 +0300

    update tests

--- backend/tests/modules/auth/model.js
+++ backend/tests/modules/auth/model.js
@@ -1,39 +1,29 @@
-const options = {
-  mongodb: {
-    protocol: 'mongodb://',
-    user: 'root',
-    password: 'root',
-    host: 'localhost',
-    port: '27017',
-    authSource: 'admin',
-    dbname: 'hawk',
-    collection: 'test',
-    get connection() {
-      if (this.user && this.password && this.authSource) {
-        return `${this.protocol + this.user}:${this.password}@${this.host}:${this.port}/${this.dbname}?authSource=${this.authSource}`;
-      }
-      return `${this.protocol + this.host}:${this.port}/${this.dbname}`;
-    }
-  }
+const mongodbOptions = {
+  protocol: 'mongodb://',
+  user: 'root',
+  password: 'root',
+  host: 'localhost',
+  port: '27017',
+  authSource: 'admin',
+  dbName: 'hawk'
 };
+const utils = require('../../../modules/auth/utils');
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(options.mongodb.connection, {useNewUrlParser: true});
+mongoose.connect(utils.getMongoUrl(mongodbOptions), {useNewUrlParser: true});
 
 const UserModel = require('../../../modules/auth/model')();
 
 const chai = require('chai');
 const assert = chai.assert;
 
-describe('MODULE.AUTH', function () {
-  describe('Testing auth model', function () {
-    it('Try to add new user with email test@example.com', function () {
-      return UserModel.add('test@example.com').then((user) => {
-        assert.isObject(user, 'result must be object');
-        assert(user._id && user.email === 'test@example.com' && user.password);
-      });
+module.exports = function () {
+  it('Try to add new user with email test@example.com', function () {
+    return UserModel.add('test@example.com').then((user) => {
+      assert.isObject(user, 'result must be object');
+      assert(user._id && user.email === 'test@example.com' && user.password, 'user object is invalid');
     });
   });
-});
+};
