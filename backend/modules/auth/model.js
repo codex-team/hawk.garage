0d4ea971c7adb630ed06de676b8b1b11b5af7f25
Nikita Melnikov <nikmel2803@gmail.com>
2019-01-20 00:45:47 +0300

    create docs

--- backend/modules/auth/model.js
+++ backend/modules/auth/model.js
@@ -10,7 +10,22 @@ const userSchema = new Schema({
 const mongooseUserModel = mongoose.model('user', userSchema);
 
 module.exports = function (params) {
+  /**
+   * @typedef {Object} User
+   * @property {string} _id - user id
+   * @property {string} email - user email
+   * @property {string} password - hashed user password
+   */
+
+  /**
+   * @class UserModel - model for working with database
+   */
   class UserModel {
+    /**
+     * Add new user to database
+     * @param {string} email - user email
+     * @returns {Promise<User>} - added user
+     */
     add(email) {
       const password = utils.generatePassword();
       const passwordHashed = utils.generateHash(password);
