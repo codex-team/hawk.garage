f46602686500293586990cd0e4cb4c4f69cd30c4
Nikita Melnikov <nikmel2803@gmail.com>
2019-01-22 14:27:31 +0300

    fix documentation

--- backend/modules/auth/model.js
+++ backend/modules/auth/model.js
@@ -9,34 +9,32 @@ const userSchema = new Schema({
 
 const mongooseUserModel = mongoose.model('user', userSchema);
 
-module.exports = function (params) {
-  /**
-   * @typedef {Object} User
-   * @property {string} _id - user id
-   * @property {string} email - user email
-   * @property {string} password - hashed user password
-   */
+/**
+ * @typedef {Object} User
+ * @property {string} _id - user id
+ * @property {string} email - user email
+ * @property {string} password - hashed user password
+ */
 
+/**
+ * @class UserModel - model for working with database
+ */
+class UserModel {
   /**
-   * @class UserModel - model for working with database
+   * Add new user to database
+   * @param {string} email - user email
+   * @returns {Promise<User>} - added user
    */
-  class UserModel {
-    /**
-     * Add new user to database
-     * @param {string} email - user email
-     * @returns {Promise<User>} - added user
-     */
-    add(email) {
-      const password = utils.generatePassword();
-      const passwordHashed = utils.generateHash(password);
-      const user = {
-        email,
-        password: passwordHashed
-      };
+  static async add(email) {
+    const password = utils.generatePassword();
+    const passwordHashed = await utils.generateHash(password);
+    const user = {
+      email,
+      password: passwordHashed
+    };
 
-      return mongooseUserModel.create(user);
-    }
+    return mongooseUserModel.create(user);
   }
+}
 
-  return new UserModel();
-};
+module.exports = UserModel;
