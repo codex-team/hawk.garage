12a0eafca209799468e5a494e6a4522f00f88c6e
Nikita Melnikov <nikmel2803@gmail.com>
2019-01-22 14:39:04 +0300

    replace .env file

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-require('dotenv').config();
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const publicDir = path.resolve(__dirname, '../frontend');
 const templatesPath = path.resolve(__dirname, '../frontend/yard/views');
 
