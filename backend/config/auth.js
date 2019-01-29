c4f225e99c46f4ff68588832c3f14cf09fef0d96
Nikita Melnikov <nikmel2803@gmail.com>
2019-01-29 21:03:42 +0300

    remove mongoDB config

--- backend/config/auth.js
+++ backend/config/auth.js
@@ -1,11 +1,4 @@
 module.exports = {
-  mongodb: { // MongoDB connection params
-    protocol: 'mongodb://',
-    host: 'localhost',
-    port: '27017',
-    dbname: 'hawk',
-    collection: 'users'
-  },
   routePrefix: '/', // ex: if / => route is /sign-up, if /auth => route is /auth/sign-up
   signUp: { // params for sign-up route
     viewPath: 'auth/sign-up', // path to view
