a06bc53f734310285a5e44782522689e2bf86be7
Nikita Melnikov <nikmel2803@gmail.com>
2019-01-29 21:09:03 +0300

    code refactoring

--- backend/modules/auth/routes/signUp.js
+++ backend/modules/auth/routes/signUp.js
@@ -2,15 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = function (params) {
-  function get(req, res, next) {
+  router.get('/sign-up', (req, res, next) => {
     if (res.locals.user) {
       res.redirect(params.redirectIfAuthed);
       return;
     }
 
     res.render(params.viewPath);
-  }
-
-  router.get('/sign-up', get);
+  });
   return router;
 };
