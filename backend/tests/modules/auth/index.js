a80a03147443f692171c12b7b6eba2d1b9d2bcbb
Nikita Melnikov <nikmel2803@gmail.com>
2019-02-01 10:33:49 +0300

    replace getMongoUrl function

--- backend/tests/modules/auth/index.js
+++ backend/tests/modules/auth/index.js
@@ -1,3 +1,19 @@
+const mongoose = require('mongoose');
+const utils = require('../../../modules/utils');
+
+const mongodbOptions = {
+  protocol: 'mongodb://',
+  user: 'root',
+  password: 'root',
+  host: 'localhost',
+  port: '27017',
+  authSource: 'admin',
+  dbName: 'hawk_test'
+};
+
+mongoose.connect(utils.getMongoUrl(mongodbOptions), {useNewUrlParser: true});
+mongoose.Promise = global.Promise;
+
 describe('MODULE.AUTH', function () {
   describe('Auth utils', require('./utils').bind(this));
   describe('Auth model', require('./model').bind(this));
