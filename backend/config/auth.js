a400b57bcfc0ee74f7b22560f305eac301e00d45
Nikita Melnikov <nikmel2803@gmail.com>
2019-02-18 07:34:17 +0300

    add email settings in auth config

--- backend/config/auth.js
+++ backend/config/auth.js
@@ -1,6 +1,13 @@
 module.exports = {
   signUp: { // params for sign-up route
     viewPath: 'auth/sign-up', // path to view
+    email: {
+      subject: 'Welcome to Hawk.so',
+      templatePath: 'notifies/email/join',
+      renderParams: {
+        settingsLink: process.env.SERVER_URL + '/garage/settings'
+      }
+    },
     redirect: '/garage'
   }
 };
