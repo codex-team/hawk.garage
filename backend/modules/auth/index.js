8aa008fcd66117ea4cc0f1507d7f8281c4e3b160
Nikita Melnikov <nikmel2803@gmail.com>
2019-02-26 12:23:59 +0300

    add markup

--- backend/modules/auth/index.js
+++ backend/modules/auth/index.js
@@ -5,7 +5,9 @@ const router = express.Router();
  * Router setup
  */
 const signUp = require('./routes/signUp');
+const login = require('./routes/login');
 
+router.use('/', login);
 router.use('/', signUp);
 
 module.exports = router;
