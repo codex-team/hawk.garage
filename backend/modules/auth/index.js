ed0b943f596401c99462f9e5e888fe5c5d0efff6
Nikita Melnikov <nikmel2803@gmail.com>
2019-03-04 23:12:29 +0300

    install jwt lib

--- backend/modules/auth/index.js
+++ backend/modules/auth/index.js
@@ -2,24 +2,24 @@ const express = require('express');
 const router = express.Router();
 const UserModel = require('./model');
 
-router.use('/', async function (req, res, next) {
-  res.locals.user = {};
-
-  const userEmail = req.cookies.email;
-  const userPassword = req.cookies.password;
-
-  try {
-    const user = await UserModel.checkUser(userEmail, userPassword);
-
-    if (user) {
-      res.locals.user = user;
-    }
-    next();
-  } catch (e) {
-    console.log('Error while getting user from DB', e);
-    next();
-  }
-});
+// router.use('/', async function (req, res, next) {
+//   res.locals.user = {};
+//
+//   const userEmail = req.cookies.email;
+//   const userPassword = req.cookies.password;
+//
+//   try {
+//     const user = await UserModel.checkUser(userEmail, userPassword);
+//
+//     if (user) {
+//       res.locals.user = user;
+//     }
+//     next();
+//   } catch (e) {
+//     console.log('Error while getting user from DB', e);
+//     next();
+//   }
+// });
 
 /**
  * Router setup
--- backend/modules/auth/routes/login.js
+++ backend/modules/auth/routes/login.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const UserModel = require('../model');
+const jwt = require('jsonwebtoken');
 
 router.get('/login', (req, res) => {
   if (res.locals.user) {
@@ -29,6 +30,14 @@ router.post('/login', async (req, res) => {
         }
       });
     }
+
+    const oneMinute = Math.floor(Date.now() / 1000) + (60 * 60);
+    const token = jwt.sign({
+      exp: oneMinute,
+      userId: user._id
+    }, process.env.JWT_SECRET_STRING);
+    console.log(token);
+    res.cookie('accessToken', token, {expires: oneMinute, httpOnly: true});
     return res.redirect('/garage');
   } catch (e) {
     console.log('Error while getting user from DB', e);
