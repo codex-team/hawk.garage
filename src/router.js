47a8122061b774624df1a90e815bc0b402361e2b
Nikita Melnikov <nikmel2803@gmail.com>
2019-03-31 19:04:19 +0300

    update router: remove base path & redirect from / to /sign-up

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './pages/Home.vue';
 import Settings from './pages/Settings.vue';
 import SignUp from './pages/SignUp.vue';
 
@@ -8,12 +7,11 @@ Vue.use(Router);
 
 export default new Router({
   mode: 'history',
-  base: '/garage',
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      redirect: '/sign-up'
     },
     {
       path: '/settings',
