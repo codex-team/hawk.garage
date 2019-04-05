edb1cfa869f7b6c8583cb64029c3484f588421b6
Daniil <beltyukovdanil@gmail.com>
2019-04-05 07:59:09 +0300

    Bootstrap api

---
+++ src/store/modules/auth.js
@@ -0,0 +1,24 @@
+import auth from '../modules/auth';
+
+const state = {
+  user: ''
+};
+
+const getters = {};
+
+const actions = {
+  login({ commit, state }, { email, password }) {},
+  logout({ commit }) {},
+  signUp({ commit, state }, { email }) {}
+};
+
+const mutations = {
+  setUser(state, username) {}
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+};
