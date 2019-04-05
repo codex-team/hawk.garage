9fde71d5fe2f88048dc4e11edd4b35b8d8c2e89b
dyadyaJora <ja_massa97@mail.ru>
2019-04-06 00:17:18 +0300

    image caching

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,6 +8,21 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
+workbox.routing.registerRoute(
+  /\.(?:png|jpg|jpeg|svg|gif)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'hawk.garage-image-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        // Cache only 20 images.
+        maxEntries: 20,
+        // Cache for a maximum of a week.
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      })
+    ],
+  })
+);
+
 if (workbox) {
   console.log(`Yay! Workbox is loaded`);
 } else {
