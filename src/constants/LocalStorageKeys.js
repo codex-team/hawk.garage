2b0081a7ad09b7a49fd104619f70f52cbde530e4
dyadyaJora <ja_massa97@mail.ru>
2019-04-08 19:48:12 +0300

    add const description

--- src/constants/LocalStorageKeys.js
+++ src/constants/LocalStorageKeys.js
@@ -1,1 +1,6 @@
-export const PROJECTS_KEY = 'p';
+/**
+ * @constant {string} PROJECTS_KEY
+ * This constant stores the name of the key
+ * that is used to write data about the projects in localStorage
+ */
+export const PROJECTS_KEY = 'projects';
