6f4e43e20390e6a311f38eb6b19f8aab8f3be39f
Nikita Melnikov <nikmel2803@gmail.com>
2019-04-25 10:46:16 +0300

    delete vue-axios

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import Vue from 'vue';
-import axios from 'axios';
-import VueAxios from 'vue-axios';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-
-Vue.use(VueAxios, axios);
 
 Vue.config.devtools = process.env.NODE_ENV !== 'production';
 Vue.config.debug = process.env.NODE_ENV !== 'production';
