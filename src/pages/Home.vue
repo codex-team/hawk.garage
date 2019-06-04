d7b87dbbee6fc590d7a417831baafa72e9fedb4c
Nikita Melnikov <nikmel2803@gmail.com>
2019-06-04 19:57:54 +0300

    base layout

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,21 +1,22 @@
 <template>
   <div class="home">
-    <aside class="home__aside">
-      <div class="home__aside-header">
-        <button @click="logout">Logout</button>
-        <button @click="changeTheme">Change theme</button>
+    <aside class="aside">
+      <div class="aside__header">
+        <div class="aside__user-picture"></div>
+        <div class="aside__hawk-title">Hawk</div>
+        <div class="aside__user-email">taly@codex.so</div>
       </div>
-      <div class="home__workspaces">
+      <div class="aside__workspaces">
         <img
-          class="home__workspace-item"
+          class="aside__workspace-item"
           v-for="workspace in workspaces"
           :key="workspace.id"
           :src="workspace.picture"
         >
       </div>
-      <div class="home__projects">
+      <div class="aside__projects">
         <div
-          class="home__project-item"
+          class="aside__project-item"
           v-for="project in projects"
           :key="project.id"
         >
@@ -74,13 +75,39 @@ export default {
     display: flex;
     min-height: 100%;
 
-    &__aside {
-      background-color: var(--color-bg-main);
-      min-width: 250px;
-    }
-
     &__content {
       width: 100%;
+    }
+  }
+
+  .aside {
+    background-color: var(--color-bg-main);
+    min-width: 250px;
+    padding: 20px;
+
+    &__header {
+      /*display: flex;*/
+      /*flex-direction: column;*/
+    }
+
+    &__user-picture {
+      float: right;
+      width: 40px;
+      height: 40px;
+      border-radius: 11px;
+      background: url("https://capella.pics/a45c947c-8708-4d80-8ca2-e60f4d404bd8.jpg") center center;
+      background-size: cover;
+    }
+
+    &__hawk-title {
+      color: var(--color-text-main);
+      font-weight: bold;
+      letter-spacing: 0.19px;
+    }
+
+    &__user-email {
+      color: var(--color-text-second);
+      font-size: 14px;
     }
 
     &__workspace-item {
