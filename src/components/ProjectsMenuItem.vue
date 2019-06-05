5b5b66d8c13c09d8901cfc5e9b25b956a79f15b9
Nikita Melnikov <nikmel2803@gmail.com>
2019-06-05 21:37:58 +0300

    improve layout

--- src/components/ProjectsMenuItem.vue
+++ src/components/ProjectsMenuItem.vue
@@ -6,11 +6,13 @@
       class="project-menu-item__picture"
       :style="{ backgroundImage: `url('${project.picture}')` }"
     ></div>
-    <div class="project-menu-item__name">
-      {{project.name}}
-    </div>
-    <div class="project-menu-item__last-event">
-      Uncaught TypeError: Cannot read property 'activate' of undefined
+    <div class="project-menu-item__info">
+      <div class="project-menu-item__name">
+        {{project.name}}
+      </div>
+      <div class="project-menu-item__last-event">
+        Uncaught TypeError: Cannot read property 'activate' of undefined фвцфцвфцвфцв
+      </div>
     </div>
     <div class="project-menu-item__events-number">
       343
@@ -36,8 +38,8 @@ export default {
     max-height: 62px;
     padding: 6px 20px;
     display: flex;
-    flex-flow: column wrap;
-    align-content: flex-start;
+    justify-content: flex-start;
+    align-items: stretch;
 
     &__name {
       font-size: 14px;
@@ -48,9 +50,15 @@ export default {
     &__last-event {
       font-size: 12.6px;
       line-height: 1.27;
+      height: 30px;
       letter-spacing: 0.1px;
       color: var(--color-text-second);
       width: 206px;
+
+      -webkit-line-clamp: 2;
+      overflow: hidden;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
     }
 
     &__picture {
@@ -59,20 +67,20 @@ export default {
       border-radius: 10px;
       background-size: cover;
       background-position: center;
-      flex-basis: 100%;
-      margin-bottom: 36px;
       margin-right: 15px;
     }
 
     &__events-number {
-      /*margin-left: 15px;*/
       max-width: 42px;
       background-color: var(--color-indicator-medium);
       border-radius: 7px;
       padding: 4px 7px 3px 7px;
       font-size: 11px;
-      /*margin-left: auto;*/
-      align-self: flex-end;
+      margin: auto 0 auto auto;
+    }
+
+    &:hover {
+      background-color: var(--color-bg-second);
     }
   }
 </style>
