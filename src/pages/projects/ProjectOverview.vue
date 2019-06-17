b8b967bdeee6054b500677ba67b95f6153efce63
Nikita Melnikov <nikmel2803@gmail.com>
2019-06-17 17:37:34 +0300

    header and chart layout

--- src/pages/projects/ProjectOverview.vue
+++ src/pages/projects/ProjectOverview.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="project-overview">
     <div class="project-overview__header">
-      {{project.name}}
+      <div
+        class="project-overview__icon"
+        :style="{ backgroundImage: `url('${project.picture}')` }"
+      ></div>
+      <div class="project-overview__name">
+        {{project.name}}
+      </div>
+      <div class="project-overview__notifications"></div>
     </div>
     <div class="project-overview__chart">
 
@@ -33,5 +40,36 @@ export default {
   .project-overview {
     display: flex;
     flex-direction: column;
+    background-color: var(--color-bg-second);
+    height: 100%;
+
+    &__header {
+      display: flex;
+      align-items: center;
+      height: 50px;
+      box-shadow: 0 1px 5px 0 var(--color-bg-main), inset 0 1px 3px 0 rgba(0, 0, 0, 0.5);
+    }
+
+    &__icon {
+      width: 26px;
+      height: 26px;
+      background-size: cover;
+      background-position: center center;
+      border-radius: 10px;
+      margin-left: 15px;
+    }
+
+    &__name {
+      color: var(--color-text-main);
+      font-weight: bold;
+      font-size: 15px;
+      margin-left: 10px;
+    }
+
+    &__chart {
+      height: 215px;
+      background-color: var(--color-bg-main);
+      margin: 16px 15px 15px;
+    }
   }
 </style>
