a9b6a46afafd5ad4341d533d5fb8530174b002fa
Nikita Melnikov <nikmel2803@gmail.com>
2019-06-17 15:54:32 +0300

    create project overview component

---
+++ src/pages/projects/ProjectOverview.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="project-overview">Project overview</div>
+</template>
+
+<script>
+export default {
+  name: 'ProjectOverview'
+};
+</script>
+
+<style>
+.project-overview {
+  display: flex;
+  flex-direction: column;
+}
+</style>
