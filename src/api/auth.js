ecadf3c7226987a7f178bd3f09c293ce7bb26a68
Nikita Melnikov <nikmel2803@gmail.com>
2019-06-19 13:03:33 +0300

    update documentation

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,6 +2,12 @@ import { MUTATION_LOGIN, MUTATION_SIGN_UP } from './mutations';
 import * as api from './index';
 
 /**
+ * @typedef {Object} TokensPair
+ * @property {string} accessToken - user's access token
+ * @property {string} refreshToken - user's refresh token for getting new tokens pair
+ */
+
+/**
  * Enum of auth module errors
  * @enum {String}
  */
@@ -16,8 +22,7 @@ export const AUTH_ERROR = {
  *
  * @param {string} email - Email
  * @param {string} password - Password
- * @returns {Promise<string>} - Auth token
- * @throws {Error} Authentication error occurred
+ * @returns {Promise<TokensPair>} - Auth token
  */
 export async function login(email, password) {
   return (await api.call(MUTATION_LOGIN, { email, password })).login;
@@ -27,8 +32,7 @@ export async function login(email, password) {
  * Sign up by email and return status(ok)
  *
  * @param {string} email - Email
- * @returns {Promise<boolean>} Response status
- * @throws {Error} Authentication error occured
+ * @returns {Promise<TokensPair>} Response status
  */
 export async function signUp(email) {
   return (await api.call(MUTATION_SIGN_UP, { email })).signUp;
