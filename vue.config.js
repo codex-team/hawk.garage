3617e1ac2a254f3e6da46717fe68a8eefb323798
Nikita Melnikov <nikmel2803@gmail.com>
2019-06-23 13:25:10 +0300

    remove graphql loader

--- vue.config.js
+++ vue.config.js
@@ -9,14 +9,5 @@ module.exports = {
       swSrc: 'public/service-worker.js'
     }
   },
-  chainWebpack: config => {
-    // GraphQL Loader
-    config.module
-      .rule('graphql')
-      .test(/\.graphql$/)
-      .use('webpack-graphql-loader')
-      .loader('webpack-graphql-loader')
-      .end();
-  },
   assetsDir: 'static'
 };
