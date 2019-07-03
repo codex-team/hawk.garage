4d3eaf4749d3798f3b1b1ad41abf1208135b112f
Nikita Melnikov <nikmel2803@gmail.com>
2019-07-03 19:56:51 +0300

    split panel to another component

---
+++ src/components/Home/Panel.vue
@@ -0,0 +1,186 @@
+<template>
+  <div class="home-panel">
+    <div
+      class="home-panel__user-picture"
+      @click="logout"
+    ></div>
+    <hr class="home-panel__delimiter">
+    <div class="home-panel__button-create-wrapper">
+      <div
+        class="home-panel__button-create"
+        @click="$router.push('/workspaces/create')"
+      >
+        <Icon symbol="plus"></Icon>
+      </div>
+    </div>
+    <hr class="home-panel__delimiter">
+    <div class="home-panel__workspaces-menu" v-if="workspaces.length">
+      <div ref="workspaceHighlight" class="home-panel__workspace-highlight"></div>
+      <WorkspacesMenuItem
+        v-for="workspace in workspaces"
+        :key="workspace.id"
+        :workspace="workspace"
+        :active="activeWorkspaceId === workspace.id"
+        class="home-panel__workspace-item"
+        @click.native="onWorkspaceItemClick($event, workspace.id)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+
+import { RESET_STORE } from '../../store/actions';
+import Icon from '../Icon';
+import WorkspacesMenuItem from './WorkspacesMenuItem';
+
+export default {
+  name: 'Panel',
+  components: {
+    WorkspacesMenuItem,
+    Icon
+  },
+  data() {
+    return {
+      /**
+       * Current workspace id
+       */
+      activeWorkspaceId: null
+    };
+  },
+  methods: {
+    /**
+     * Logouts user
+     */
+    logout() {
+      this.$store.dispatch(RESET_STORE);
+    },
+
+    /**
+     * Works when workspace item is clicked
+     */
+    onWorkspaceItemClick($event, workspaceId) {
+      this.activeWorkspaceId = workspaceId;
+      const highLightPadding = 9;
+
+      this.$refs.workspaceHighlight.style.top =
+          $event.target.offsetTop - highLightPadding + 'px';
+    }
+  },
+  computed: {
+    /**
+     * @return {Array<Workspace>} - registered workspaces
+     */
+    workspaces() {
+      return this.$store.state.workspaces.list;
+    }
+  }
+};
+</script>
+
+<style>
+  .home-panel {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 76px;
+
+    background-color: #1a1d26;
+
+    &__user-picture {
+      width: 36px;
+      height: 36px;
+      margin-top: 20px;
+      margin-bottom: 18px;
+
+      background: url("https://capella.pics/a45c947c-8708-4d80-8ca2-e60f4d404bd8.jpg") center center;
+      background-size: cover;
+      border-radius: 10px;
+    }
+
+    &__button-create-wrapper {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding: 20px 0;
+    }
+
+    &__delimiter {
+      width: 36px;
+      margin: 0;
+
+      border: 1px solid rgba(219, 230, 255, 0.1);
+    }
+
+    &__button-create {
+      box-sizing: border-box;
+      width: 36px;
+      height: 36px;
+      margin: auto;
+
+      background-color: #1a1d26;
+      border: solid 1px var(--color-text-main);
+      border-radius: 9px;
+      cursor: pointer;
+
+      .icon {
+        width: 16px;
+        height: 16px;
+        padding: 10px;
+      }
+    }
+
+    &__workspaces-menu {
+      position: relative;
+
+      margin-top: 20px;
+    }
+
+    &__workspace-item {
+      position: relative;
+      z-index: 10;
+
+      margin-bottom: 20px;
+    }
+
+    &__workspace-highlight {
+      position: absolute;
+      top: -9px;
+      z-index: 0;
+
+      width: 65px;
+      height: 54px;
+      margin-left: -9px;
+
+      background: var(--color-bg-main);
+      border-top-left-radius: 10px;
+      border-bottom-left-radius: 10px;
+
+      transition: top 150ms cubic-bezier(.37, -0.19, .42, 1.39);
+
+      &:before,
+      &:after {
+        position: absolute;
+        right: 0;
+
+        display: block;
+        width: 10px;
+        height: 10px;
+
+        content: '';
+      }
+
+      &:before {
+        top: -10px;
+
+        background-image: radial-gradient(circle at 0 0, transparent 9px, var(--color-bg-main) 10px);
+      }
+
+      &:after {
+        bottom: -10px;
+
+        background-image: radial-gradient(circle at 0 100%, transparent 9px, var(--color-bg-main) 10px);
+      }
+    }
+  }
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,31 +1,7 @@
 <template>
   <div class="home">
     <aside class="aside">
-      <div class="aside__left-column">
-        <div
-          class="aside__user-picture"
-          @click="logout"
-        ></div>
-        <div class="aside__button-create-wrapper">
-          <div
-            class="aside__button-create"
-            @click="$router.push('/workspaces/create')"
-          >
-            <Icon symbol="plus"></Icon>
-          </div>
-        </div>
-        <div class="aside__workspaces-menu" v-if="workspaces.length">
-          <div ref="workspaceHighlight" class="aside__workspace-highlight"></div>
-          <WorkspacesMenuItem
-            v-for="workspace in workspaces"
-            :key="workspace.id"
-            :workspace="workspace"
-            :active="activeWorkspaceId === workspace.id"
-            class="aside__workspace-item"
-            @click.native="onWorkspaceItemClick($event, workspace.id)"
-          />
-        </div>
-      </div>
+      <Panel/>
       <div class="aside__right-column">
         <div class="aside__projects-list" v-if="projects">
           <ProjectsMenuItem
@@ -45,53 +21,24 @@
 
 <script>
 
-import { RESET_STORE } from '../store/actions';
 import { THEME_CHANGE } from '../store/actions/app';
 import { FETCH_WORKSPACES } from '../store/actions/workspaces';
 import { Themes } from '../store/modules/app';
-import ProjectsMenuItem from '../components/ProjectsMenuItem';
-import Icon from '../components/Icon';
-import WorkspacesMenuItem from '../components/WorkspacesMenuItem';
+import ProjectsMenuItem from '../components/Home/ProjectsMenuItem';
+import Panel from '../components/Home/Panel';
 
 export default {
   name: 'Home',
   components: {
-    WorkspacesMenuItem,
-    ProjectsMenuItem,
-    Icon
-  },
-  data() {
-    return {
-      /**
-       * Current workspace id
-       */
-      activeWorkspaceId: null
-    };
+    Panel,
+    ProjectsMenuItem
   },
   methods: {
-    /**
-     * Logouts user
-     */
-    logout() {
-      this.$store.dispatch(RESET_STORE);
-    },
-
     /**
      * Toggles theme (dark/light)
      */
     changeTheme() {
       this.$store.commit(THEME_CHANGE, this.$store.state.app.theme === Themes.DARK ? Themes.LIGHT : Themes.DARK);
-    },
-
-    /**
-     * Works when workspace item is clicked
-     */
-    onWorkspaceItemClick($event, workspaceId) {
-      this.activeWorkspaceId = workspaceId;
-      const highLightPadding = 9;
-
-      this.$refs.workspaceHighlight.style.top =
-          $event.target.offsetTop - highLightPadding + 'px';
     }
   },
 
@@ -139,108 +86,6 @@ export default {
     display: flex;
 
     background-color: var(--color-bg-main);
-
-    &__left-column {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      width: 76px;
-
-      background-color: #1a1d26;
-    }
-
-    &__user-picture {
-      width: 36px;
-      height: 36px;
-      margin-top: 20px;
-      margin-bottom: 18px;
-
-      background: url("https://capella.pics/a45c947c-8708-4d80-8ca2-e60f4d404bd8.jpg") center center;
-      background-size: cover;
-      border-radius: 10px;
-    }
-
-    &__button-create-wrapper {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      margin: 20px;
-      padding: 20px;
-
-      border-top: 1px solid rgba(219, 230, 255, 0.1);
-      border-bottom: 1px solid rgba(219, 230, 255, 0.1);
-    }
-
-    &__button-create {
-      box-sizing: border-box;
-      width: 36px;
-      height: 36px;
-      margin: auto;
-
-      background-color: #1a1d26;
-      border: solid 1px var(--color-text-main);
-      border-radius: 9px;
-      cursor: pointer;
-
-      .icon {
-        width: 16px;
-        height: 16px;
-        padding: 10px;
-      }
-    }
-
-    &__workspaces-menu {
-      position: relative;
-
-      padding: 0 20px 24px;
-    }
-
-    &__workspace-item {
-      position: relative;
-      z-index: 10;
-
-      margin-bottom: 20px;
-    }
-
-    &__workspace-highlight {
-      position: absolute;
-      top: -9px;
-      z-index: 0;
-
-      width: 65px;
-      height: 54px;
-      margin-left: -9px;
-
-      background: var(--color-bg-main);
-      border-top-left-radius: 10px;
-      border-bottom-left-radius: 10px;
-
-      transition: top 150ms cubic-bezier(.37, -0.19, .42, 1.39);
-
-      &:before,
-      &:after {
-        position: absolute;
-        right: 0;
-
-        display: block;
-        width: 10px;
-        height: 10px;
-
-        content: '';
-      }
-
-      &:before {
-        top: -10px;
-
-        background-image: radial-gradient(circle at 0 0, transparent 9px, var(--color-bg-main) 10px);
-      }
-
-      &:after {
-        bottom: -10px;
-
-        background-image: radial-gradient(circle at 0 100%, transparent 9px, var(--color-bg-main) 10px);
-      }
-    }
 
     &__right-column {
       width: 342px;
