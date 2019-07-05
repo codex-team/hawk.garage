170ce229bf751353834cf481504ad1f6811d9bed
Nikita Melnikov <nikmel2803@gmail.com>
2019-07-05 12:47:05 +0300

    base popup component

---
+++ src/components/Popup.vue
@@ -0,0 +1,36 @@
+<template>
+  <transition>
+    <div class="popup-window">
+      <div class="popup-window__container">
+        <slot class="popup-window__content"></slot>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+  export default {
+    name: 'Popup'
+  };
+</script>
+
+<style>
+.popup-window {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 10;
+
+  display: flex;
+
+  &__container {
+    display: flex;
+  }
+
+  &__content {
+    margin: auto;
+  }
+}
+</style>
--- src/components/SettingsWindow.vue
+++ src/components/SettingsWindow.vue
@@ -1,80 +1,76 @@
 <template>
-  <div class="settings-window">
-    <div class="settings-window__menu">
-      <div class="settings-window__header window-header clearfix">
-        <div class="window-header__logo"></div>
-        <div class="window-header__title">
-          Hawk
+  <PopupWindow>
+    <div class="settings-window">
+      <div class="settings-window__menu">
+        <div class="settings-window__header window-header clearfix">
+          <div class="window-header__logo"></div>
+          <div class="window-header__title">
+            Hawk
+          </div>
+          <div class="window-header__caption">
+            Made by CodeX
+          </div>
         </div>
-        <div class="window-header__caption">
-          Made by CodeX
+        <div class="settings-window__menu-item">
+          Account settings
+        </div>
+        <div class="settings-window__menu-item">
+          Notifications
+        </div>
+        <div class="settings-window__menu-item">
+          Billing information
+        </div>
+        <hr class="settings-window__menu-delimiter">
+        <div class="settings-window__menu-item">
+          Appearance
+        </div>
+        <div class="settings-window__menu-item">
+          Language
+        </div>
+        <hr class="settings-window__menu-delimiter">
+        <div
+          class="settings-window__menu-item settings-window__menu-item--attention"
+          @click="logout"
+        >
+          Log out
         </div>
       </div>
-      <div class="settings-window__menu-item">
-        Account settings
-      </div>
-      <div class="settings-window__menu-item">
-        Notifications
-      </div>
-      <div class="settings-window__menu-item">
-        Billing information
-      </div>
-      <hr class="settings-window__menu-delimiter">
-      <div class="settings-window__menu-item">
-        Appearance
-      </div>
-      <div class="settings-window__menu-item">
-        Language
-      </div>
-      <hr class="settings-window__menu-delimiter">
+      <div class="settings-window__content"></div>
       <div
-        class="settings-window__menu-item settings-window__menu-item--attention"
-        @click="logout"
+        class="settings-window__exit-button exit-button"
+        @click="$router.push('/')"
       >
-        Log out
+        <Icon class="exit-button__icon" symbol="close"></Icon>
+        <div class="exit-button__key">ESC</div>
       </div>
     </div>
-    <div class="settings-window__content"></div>
-    <div
-      class="settings-window__exit-button exit-button"
-      @click="$router.push('/')"
-    >
-      <Icon class="exit-button__icon" symbol="close"></Icon>
-      <div class="exit-button__key">ESC</div>
-    </div>
-  </div>
+  </PopupWindow>
 </template>
 
 <script>
-import { RESET_STORE } from '../store/actions';
-import Icon from './Icon';
+  import { RESET_STORE } from '../store/actions';
+  import Icon from './Icon';
+  import Popup from './Popup';
 
-export default {
-  name: 'SettingsWindow',
-  methods: {
-    /**
-     * Logouts user
-     */
-    logout() {
-      this.$store.dispatch(RESET_STORE);
+  export default {
+    name: 'SettingsWindow',
+    methods: {
+      /**
+       * Logouts user
+       */
+      logout() {
+        this.$store.dispatch(RESET_STORE);
+      }
+    },
+    components: {
+      Icon,
+      PopupWindow: Popup
     }
-  },
-  components: {
-    Icon
-  }
-};
+  };
 </script>
 
 <style>
   .settings-window {
-    position: fixed;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-
-    z-index: 10;
-
     display: flex;
 
     background-color: var(--color-bg-second);
