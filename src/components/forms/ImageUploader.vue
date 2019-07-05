78c86ca497a3da41ec2b67e58fa4cd7993d7ddfa
Nikita Melnikov <nikmel2803@gmail.com>
2019-07-05 20:53:27 +0300

    create image uploader component

---
+++ src/components/forms/ImageUploader.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="form-image-uploader">
+    UPLOAD LOGO
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormImageUploader'
+};
+</script>
+
+<style>
+  .form-image-uploader {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 60px;
+    height: 60px;
+    color: var(--color-text-second);
+    font-weight: bold;
+    font-size: 10px;
+    line-height: 1.3;
+    text-align: center;
+    border: 1px dashed var(--color-text-second);
+    border-radius: 10px;
+    user-select: none;
+  }
+</style>
