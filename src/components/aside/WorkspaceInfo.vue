87e55aff34e4688690c43fc2017bf1fe01a37c51
Nikita Melnikov <nikmel2803@gmail.com>
2019-07-10 17:59:02 +0300

    code style fix

--- src/components/aside/WorkspaceInfo.vue
+++ src/components/aside/WorkspaceInfo.vue
@@ -1,22 +1,22 @@
 <template>
-  <div class="workspace-info clearfix">
-    <EntityImage
-      class="workspace-info__image"
-      :name="workspace.name"
-      :image="workspace.image"
-      :id="workspace.id"
-    />
-    <Icon
-      class="workspace-info__project-creation-button"
-      symbol="plus"
-    />
-    <div class="workspace-info__name">
-      {{workspace.name}}
+    <div class="workspace-info clearfix">
+      <EntityImage
+        class="workspace-info__image"
+        :name="workspace.name"
+        :image="workspace.image"
+        :id="workspace.id"
+      />
+      <Icon
+        class="workspace-info__project-creation-button"
+        symbol="plus"
+      />
+      <div class="workspace-info__name">
+        {{workspace.name}}
+      </div>
+      <div class="workspace-info__settings-link">
+        Workspace settings
+      </div>
     </div>
-    <div class="workspace-info__settings-link">
-      Workspace settings
-    </div>
-  </div>
 </template>
 
 <script>
@@ -42,35 +42,35 @@ export default {
 </script>
 
 <style>
-.workspace-info {
-  height: 36px;
-  font-size: 14px;
-  line-height: 16px;
+  .workspace-info {
+    height: 36px;
+    font-size: 14px;
+    line-height: 16px;
 
-  &__image {
-    float: left;
-    width: 36px;
-    margin-right: 15px;
-  }
+    &__image {
+      float: left;
+      width: 36px;
+      margin-right: 15px;
+    }
 
-  &__name {
-    font-weight: 600;
-  }
+    &__name {
+      font-weight: 600;
+    }
 
-  &__settings-link {
-    margin-top: 2px;
-    color: var(--color-text-second);
-  }
+    &__settings-link {
+      margin-top: 2px;
+      color: var(--color-text-second);
+    }
 
-  &__project-creation-button {
-    position: relative;
-    top: 5px;
-    float: right;
-    width: 26px;
-    height: 26px;
-    padding: 6px;
-    background-color: var(--color-indicator-medium);
-    border-radius: var(--border-radius);
+    &__project-creation-button {
+      position: relative;
+      top: 5px;
+      float: right;
+      width: 26px;
+      height: 26px;
+      padding: 6px;
+      background-color: var(--color-indicator-medium);
+      border-radius: var(--border-radius);
+    }
   }
-}
 </style>
