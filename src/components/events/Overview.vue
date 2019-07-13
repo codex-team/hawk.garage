3f9227be4d35e0abea04dbbb5b8e08e3f8cb605c
Nikita Melnikov <nikmel2803@gmail.com>
2019-07-13 20:56:27 +0300

    base event overview component

---
+++ src/components/events/Overview.vue
@@ -0,0 +1,30 @@
+<template>
+  <PopupDialog>
+
+  </PopupDialog>
+</template>
+
+<script>
+import PopupDialog from '../utils/PopupDialog';
+
+export default {
+  name: 'EventOverview',
+  data() {
+    const projectId = this.$route.params.projectId;
+    const eventId = this.$route.params.eventId;
+    const event = this.$store.getters.project(projectId).events.find(ev => ev.id === eventId);
+
+    return {
+      event,
+      projectId
+    };
+  },
+  components: {
+    PopupDialog
+  }
+};
+</script>
+
+<style>
+
+</style>
