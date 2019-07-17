a66477c8a7502355923c47c8c7c1bd6554734491
Nikita Melnikov <nikmel2803@gmail.com>
2019-07-17 10:39:08 +0300

    create catalog-item component

---
+++ src/components/projects/CatalogItem.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="catalog-item">
+    <div
+      class="catalog-item__header"
+      :style="{'background-image': `url(${image})`}"
+    >
+      <Badge content="CATCHER"/>
+    </div>
+    <div class="catalog-item__info">
+      <div class="catalog-item__name">
+        {{ name }}
+      </div>
+      <div class="catalog-item__description">
+        {{ description }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Badge from '../utils/Badge';
+export default {
+  name: 'CatalogItem',
+  components: {
+    Badge
+  },
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+.catalog-item {
+  max-width: 230px;
+  background-color: var(--color-bg-main);
+  border-radius: 4px;
+
+  &__header {
+    height: 100px;
+  }
+
+  &__info {
+    padding: 15px;
+  }
+
+  &__name {
+    margin-bottom: 10px;
+    font-weight: bold;
+    font-size: 12px;
+    letter-spacing: 0.15px;
+  }
+
+  &__description {
+    font-size: 13px;
+    line-height: 1.38;
+  }
+}
+</style>
