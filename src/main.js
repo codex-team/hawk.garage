68af053f8e64d2a5e7bfd24a969163691c32d9e0
Nikita Melnikov <nikmel2803@gmail.com>
2019-08-25 01:27:17 +0300

    base i18n config

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store';
 import './filters';
 import './directives';
 import './registerServiceWorker';
+import i18n from './i18n';
 
 Vue.config.devtools = process.env.NODE_ENV !== 'production';
 Vue.config.debug = process.env.NODE_ENV !== 'production';
@@ -12,5 +13,6 @@ Vue.config.debug = process.env.NODE_ENV !== 'production';
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app');
