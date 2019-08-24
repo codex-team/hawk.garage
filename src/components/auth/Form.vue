0546e63562780f42106676bc743027d1cf0b3c99
Danil Beltyukov <beltyukovdanil@gmail.com>
2019-08-24 23:50:07 +0300

    add auth endpoints links

--- src/components/auth/Form.vue
+++ src/components/auth/Form.vue
@@ -1,35 +1,26 @@
 <template>
   <div class="auth-form">
-    <router-link
-      class="auth-form__title"
-      to="/"
-    >
-      Hawk
-    </router-link>
-    <div class="auth-form__caption">
-      Time for quality
+    <div class="auth-form__block-title">
+      <router-link class="auth-form__title" to="/">Hawk</router-link>
+      <br />
+      <div class="auth-form__caption">Time for quality</div>
+    </div>
+    <div class="auth-form__social-block">
+      <a class="auth-form__github-button" :href="$API_AUTH_GITHUB+'/auth/google'">Continue</a>
+      <a class="auth-form__google-button" :href="$API_AUTH_GOOGLE+'/auth/google'">Sign in</a>
     </div>
     <div class="auth-form__container">
       <div class="auth-form__picture" />
-      <form
-        class="auth-form__fields-container"
-        @submit.prevent="$emit('submit')"
-      >
+      <form class="auth-form__fields-container" @submit.prevent="$emit('submit')">
         <div class="auth-form__links">
-          <router-link to="/login">
-            Login
-          </router-link>
-          <router-link to="/sign-up">
-            Sign up
-          </router-link>
+          <router-link to="/login">Login</router-link>
+          <router-link to="/sign-up">Sign up</router-link>
         </div>
-        <hr class="auth-form__delimiter">
+        <hr class="auth-form__delimiter" />
         <div
           v-if="message"
           :class="`auth-form__message auth-form__message--${message.type}`"
-        >
-          {{ message.text }}
-        </div>
+        >{{ message.text }}</div>
         <TextFieldset
           v-for="(field, index) in fields"
           :key="index"
@@ -41,19 +32,12 @@
           :placeholder="field.placeholder"
         />
         <div class="auth-form__action-container">
-          <input
-            v-if="submitText"
-            class="button button--submit"
-            type="submit"
-            :value="submitText"
-          >
+          <input v-if="submitText" class="button button--submit" type="submit" :value="submitText" />
           <router-link
             v-if="altText && altLink"
             class="auth-form__alt-button"
             :to="altLink"
-          >
-            {{ altText }}
-          </router-link>
+          >{{ altText }}</router-link>
         </div>
       </form>
     </div>
@@ -61,11 +45,10 @@
 </template>
 
 <script>
-
-import TextFieldset from '../forms/TextFieldset';
+import TextFieldset from "../forms/TextFieldset";
 
 export default {
-  name: 'AuthForm',
+  name: "AuthForm",
   components: {
     TextFieldset
   },
@@ -95,138 +78,181 @@ export default {
 </script>
 
 <style>
-  .auth-form {
-    &__title {
-      display: inline-block;
-      color: #fff;
-      font-weight: bold;
-      font-size: 19px;
-    }
+.auth-form {
+  &__block-title {
+    text-align: center;
+    margin-bottom: 25px;
+  }
 
-    &__caption {
-      display: inline-block;
-      margin-left: 15px;
-      color: var(--color-text-second);
-      font-size: 14px;
-    }
+  &__title {
+    display: inline-block;
+    color: #fff;
+    font-weight: bold;
+    font-size: 19px;
+  }
 
-    &__title,
-    &__caption {
-      margin-bottom: 15px;
-    }
+  &__caption {
+    display: inline-block;
+    margin-left: 15px;
+    color: var(--color-text-second);
+    font-size: 14px;
+  }
 
-    &__container {
-      display: flex;
-      height: 345px;
-      background: var(--color-bg-second);
-      border: solid 1px #343949;
-      border-radius: 7px;
-      box-shadow: 0 10px 11px -10px rgba(13, 15, 26, 0.49);
+  &__title {
+    margin-bottom: 10px;
+  }
 
-      @media (--media-mobile) {
-        height: auto;
-      }
-    }
+  &__social-block {
+    margin-bottom: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 
-    &__picture {
-      width: 205px;
-      background-image: url("../../assets/hawk.png");
-      background-position: center center;
-      background-size: cover;
-      border-radius: inherit;
-      border-top-right-radius: 0;
-      border-bottom-right-radius: 0;
+  &__github-button {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    height: 40px;
+    border-radius: 4px;
+    background: no-repeat 15px/18px 100% url("../../assets/github.svg");
+    background-color: #101216;
+    padding-left: 44px;
+    padding-right: 15px;
+    margin-left: 15px;
+    font-family: Roboto;
+    font-size: 14.4px;
+    color: #ffffff;
+  }
 
-      @media (--media-mobile) {
-        display: none;
-      }
-    }
+  &__google-button {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    height: 40px;
+    border-radius: 4px;
+    background: no-repeat 15px/18px 100% url("../../assets/google.svg");
+    background-color: #ffffff;
+    padding-left: 44px;
+    padding-right: 15px;
+    margin-left: 15px;
+    font-family: Roboto;
+    font-size: 14.4px;
+    color: var(--color-text-dark);
+  }
 
-    &__links {
-      display: flex;
-      flex-direction: row-reverse;
-      justify-content: space-between;
-      color: var(--color-text-second);
-      font-weight: bold;
-      font-size: 18px;
+  &__container {
+    display: flex;
+    height: 345px;
+    background: var(--color-bg-second);
+    border: solid 1px #343949;
+    border-radius: 7px;
+    box-shadow: 0 10px 11px -10px rgba(13, 15, 26, 0.49);
 
-      @media (--media-mobile) {
-        font-size: 15px;
-      }
-
-      .router-link-active {
-        order: 1;
-        color: var(--color-text-main);
-      }
-
-      a:hover {
-        color: var(--color-text-main);
-      }
-    }
-
-    &__delimiter {
-      height: 1px;
-      margin: 20px 0 25px;
-      background: var(--color-text-main);
-      border: 0;
-      opacity: 0.11;
-
-      @media (--media-mobile) {
-        margin: 15px 0 20px;
-      }
-    }
-
-    &__fields-container {
-      flex-grow: 1;
-      padding: 30px 48px 35px 30px;
-
-      @media (--media-mobile) {
-        padding: 20px;
-      }
-    }
-
-    &__header {
-      margin-bottom: 25px;
-      font-weight: 800;
-      font-size: 27px;
-    }
-
-    &__message {
-      margin-top: -5px;
-      margin-bottom: 20px;
-      font-size: 17px;
-
-      &--error {
-        color: var(--color-indicator-critical);
-      }
-
-      &--notify {
-        color: var(--color-indicator-medium);
-      }
-    }
-
-    &__section {
-      margin: 0 0 20px;
-    }
-
-    &__action-container {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      margin-top: 30px;
-
-      @media (--media-mobile) {
-        margin-top: 20px;
-      }
-    }
-
-    &__alt-button {
-      color: var(--color-text-second);
-      font-size: 13px;
-
-      &:hover {
-        color: var(--color-text-main);
-      }
+    @media (--media-mobile) {
+      height: auto;
     }
   }
+
+  &__picture {
+    width: 205px;
+    background-image: url("../../assets/hawk.png");
+    background-position: center center;
+    background-size: cover;
+    border-radius: inherit;
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+
+    @media (--media-mobile) {
+      display: none;
+    }
+  }
+
+  &__links {
+    display: flex;
+    flex-direction: row-reverse;
+    justify-content: space-between;
+    color: var(--color-text-second);
+    font-weight: bold;
+    font-size: 18px;
+
+    @media (--media-mobile) {
+      font-size: 15px;
+    }
+
+    .router-link-active {
+      order: 1;
+      color: var(--color-text-main);
+    }
+
+    a:hover {
+      color: var(--color-text-main);
+    }
+  }
+
+  &__delimiter {
+    height: 1px;
+    margin: 20px 0 25px;
+    background: var(--color-text-main);
+    border: 0;
+    opacity: 0.11;
+
+    @media (--media-mobile) {
+      margin: 15px 0 20px;
+    }
+  }
+
+  &__fields-container {
+    flex-grow: 1;
+    padding: 30px 48px 35px 30px;
+
+    @media (--media-mobile) {
+      padding: 20px;
+    }
+  }
+
+  &__header {
+    margin-bottom: 25px;
+    font-weight: 800;
+    font-size: 27px;
+  }
+
+  &__message {
+    margin-top: -5px;
+    margin-bottom: 20px;
+    font-size: 17px;
+
+    &--error {
+      color: var(--color-indicator-critical);
+    }
+
+    &--notify {
+      color: var(--color-indicator-medium);
+    }
+  }
+
+  &__section {
+    margin: 0 0 20px;
+  }
+
+  &__action-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 30px;
+
+    @media (--media-mobile) {
+      margin-top: 20px;
+    }
+  }
+
+  &__alt-button {
+    color: var(--color-text-second);
+    font-size: 13px;
+
+    &:hover {
+      color: var(--color-text-main);
+    }
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -9,6 +9,9 @@ import './registerServiceWorker';
 Vue.config.devtools = process.env.NODE_ENV !== 'production';
 Vue.config.debug = process.env.NODE_ENV !== 'production';
 
+Vue.prototype.$API_AUTH_GOOGLE = process.env.VUE_APP_API_AUTH_GOOGLE;
+Vue.prototype.$API_AUTH_GITHUB = process.env.VUE_APP_API_AUTH_GITHUB;
+
 new Vue({
   router,
   store,
