5af2aeccf6199991d3600490c07e6d1a2ca5abb2
Nikita Melnikov <nikmel2803@gmail.com>
2019-08-25 13:37:31 +0300

    remove unused deps

--- vue.config.js
+++ vue.config.js
@@ -9,14 +9,6 @@ module.exports = {
       })
     ]
   },
-  chainWebpack: config => {
-    config.module
-      .rule('i18n')
-      .resourceQuery(/blockType=i18n/)
-      .type('javascript/auto')
-      .use('i18n')
-      .loader('@kazupon/vue-i18n-loader');
-  },
   pwa: {
     name: 'hawk.so',
     workboxPluginMode: 'InjectManifest',
