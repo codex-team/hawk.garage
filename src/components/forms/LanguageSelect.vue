b5278ec3c9b555839a747efe0c631d05de9dc80e
Nikita Melnikov <nikmel2803@gmail.com>
2019-08-25 15:07:02 +0300

    create language select component

---
+++ src/components/forms/LanguageSelect.vue
@@ -0,0 +1,23 @@
+<template>
+  <select
+    id=""
+    name=""
+  >
+    <option value="en">
+      English
+    </option>
+    <option value="ru">
+      Russia
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'LanguageSelect'
+};
+</script>
+
+<style scoped>
+
+</style>
