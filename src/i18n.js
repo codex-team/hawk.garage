b630f9126616886ffe78216185c512ccef412a23
Nikita Melnikov <nikmel2803@gmail.com>
2019-08-25 03:00:38 +0300

    change export format

--- src/i18n.js
+++ src/i18n.js
@@ -3,7 +3,9 @@ import VueI18n from 'vue-i18n';
 
 Vue.use(VueI18n);
 
-export default new VueI18n({
+const i18n = new VueI18n({
   locale: window.navigator.language || 'en',
   fallbackLocale: window.navigator.language || 'en'
 });
+
+export default i18n;
