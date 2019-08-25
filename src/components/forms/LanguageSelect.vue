108846f486027f0fd4e017284f061301f3bc35cf
Nikita Melnikov <nikmel2803@gmail.com>
2019-08-26 01:48:24 +0300

    reactive language changing in component

--- src/components/forms/LanguageSelect.vue
+++ src/components/forms/LanguageSelect.vue
@@ -1,8 +1,8 @@
 <template>
   <RadioButtonGroup
-    :name="name"
-    :label="label"
-    :options="options"
+    :name="formData.name"
+    :label="formData.label"
+    :options="formData.options"
     :value="language"
     @input="changeLanguage"
   />
@@ -17,27 +17,29 @@ export default {
   components: {
     RadioButtonGroup
   },
-  data() {
-    return {
-      name: 'language',
-      label: 'Interface Language',
-      options: [
-        {
-          name: 'Russia',
-          id: 'ru',
-          image: require('../../assets/ru.svg')
-        },
-        {
-          name: 'English',
-          id: 'en',
-          image: require('../../assets/uk.svg')
-        }
-      ]
-    };
+  computed: {
+    formData() {
+      return {
+        name: 'language',
+        label: this.$t('components.languageSelect.label'),
+        options: [
+          {
+            name: this.$t('components.languageSelect.ru'),
+            id: 'ru',
+            image: require('../../assets/ru.svg')
+          },
+          {
+            name: this.$t('components.languageSelect.en'),
+            id: 'en',
+            image: require('../../assets/uk.svg')
+          }
+        ]
+      };
+    },
+    ...mapState({
+      language: state => state.app.language
+    })
   },
-  computed: mapState({
-    language: state => state.app.language
-  }),
   methods: {
     changeLanguage(e) {
       this.$store.dispatch(SET_LANGUAGE, e);
@@ -47,53 +49,10 @@ export default {
 </script>
 
 <style>
-  .language-select {
-    &__option {
-      padding: 15px 0 15px 50px;
-      display: flex;
-      line-height: 28px;
-      letter-spacing: 0.19px;
-      position: relative;
-
-      &:focus {
-        color: red;
-      }
-
-      &::after {
-        margin-left: auto;
-        content: '';
-        width: 28px;
-        height: 28px;
-        border: 1px solid var(--color-bg-sidebar);
-        border-radius: 100%;
-        background: var(--color-bg-main);
-      }
-
-      &--ru {
-        background: no-repeat 0/33px 100% url("../../assets/ru.svg");
-      }
-
-      &--en {
-        background: no-repeat 0/33px 100% url("../../assets/uk.svg");
-      }
-    }
-
-    &__radio-button {
-      /*opacity: 0;*/
-
-      &:checked + label::after {
-        background-image: url("../../assets/sprite-icons/tick.svg");
-      }
-
-      &:focus + label::after {
-        color: red;
-      }
-    }
 
     &__delimiter {
       margin: 0;
       border: 1px solid var(--color-text-second);
       opacity: 0.1;
     }
-  }
 </style>
