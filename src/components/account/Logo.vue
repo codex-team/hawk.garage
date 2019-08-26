ca89f63c9385301ada2570184b3de039c532edeb
Yegor Berizhnoi <yegor.berezhnoi@3d4medical.com>
2019-08-27 01:31:11 +0300

    Add component

---
+++ src/components/account/Logo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="account-settings__logo" />
+</template>
+
+<script>
+  export default {
+    name: 'SettingsLogo'
+  };
+</script>
+
+<style scoped>
+  .account-settings {
+    &__logo {
+      float: left;
+      width: 42px;
+      height: 42px;
+      margin-right: 20px;
+      background-image: url("../../assets/hawk-logo.png");
+      background-position: center center;
+      background-size: cover;
+    }
+  }
+</style>
