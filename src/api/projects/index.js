485692aeb5df20d22e7ca10eff1924158a6b744b
Danil Beltyukov <beltyukovdanil@gmail.com>
2019-08-27 21:42:53 +0300

    notification settings api

--- src/api/projects/index.js
+++ src/api/projects/index.js
@@ -1,4 +1,9 @@
-import { MUTATION_CREATE_PROJECT, QUERY_RECENT_ERRORS } from './queries';
+import {
+  MUTATION_CREATE_PROJECT,
+  MUTATION_UPDATE_NOTIFICATION_SETTINGS,
+  QUERY_NOTIFICATION_SETTINGS,
+  QUERY_RECENT_ERRORS
+} from './queries';
 import * as api from '../index';
 
 /**
@@ -24,4 +29,46 @@ export async function createProject(projectInfo) {
  */
 export async function fetchRecentErrors(projectId) {
   return (await api.call(QUERY_RECENT_ERRORS, { projectId })).recent;
+}
+
+/**
+ * @typedef {Object} ProviderSettings
+ * @property {Boolean} enabled - provider enabled?
+ * @property {String} value - provider hook/email/etc
+ */
+
+/**
+ * @typedef {Object} NotifySettings
+ * @property {ProviderSettings} email
+ * @property {ProviderSettings} tg
+ * @property {ProviderSettings} slack
+ */
+
+/**
+ * @typedef {Object} Notify
+ * @property {number} actionType - action type. {ONLY_NEW: 1, ALL: 2, INCLUDING: 3}
+ * @property {string} words - filter words when action type is INCLUDING
+ * @property {NotifySettings} settings - notify settings
+ */
+
+/**
+ * Get notification settings
+ * @param projectId - Project ID
+ * @returns {Promise<Notify>}
+ */
+export async function notificationSettings(projectId) {
+  return (await api.call(QUERY_NOTIFICATION_SETTINGS, { projectId })).notificationSettings;
+}
+
+/**
+ * Update notificaion settings
+ * @param {string} projectId - Project ID
+ * @param {Notify} notify - Notify object
+ * @returns {Promise<Boolean>}
+ */
+export async function updateNotificationSettings(projectId, notify) {
+  return (await api.call(MUTATION_UPDATE_NOTIFICATION_SETTINGS, {
+    projectId,
+    notify
+  })).updateNotificationSettings;
 }
--- src/api/projects/queries.js
+++ src/api/projects/queries.js
@@ -31,3 +31,37 @@ export const QUERY_RECENT_ERRORS = `
     }
   }
 `;
+
+// language=GraphQL
+export const QUERY_NOTIFICATION_SETTINGS = `
+  query NotifiactionSettings($projectId: ID!){
+    notificationSettings(projectId: $projectId){
+      actionType
+      words
+      settings {
+        email {
+          enabled
+          value
+        }
+        tg {
+          enabled
+          value
+        }
+        slack {
+          enabled
+          value
+        }
+      }
+    }
+  }
+`;
+
+// language=GraphQL
+export const MUTATION_UPDATE_NOTIFICATION_SETTINGS = `
+  mutation UpdateNotificationSettings(
+    $projectId: ID!,
+    $notify: NotifyInput!
+  ){
+    updateNotificationSettings(projectId: $projectId, notify: $notify )
+  }
+`;
