85ab14a815461604df4f998a13afa7c41128df4c
Nikita Melnikov <nikmel2803@gmail.com>
2019-08-31 02:21:45 +0300

    Billing settings page

---
+++ src/components/account/Billing.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="account-billing settings-window-page">
+    <div class="settings-window-page__title">
+      {{ $t('settings.billing.title') }}
+    </div>
+    <label class="label account-billing__label">{{ $t('settings.billing.ownedWorkspaces') }}</label>
+    <WorkspaceBilling
+      v-for="workspace in workspaces"
+      :key="workspace.id"
+      :workspace="workspace"
+    />
+  </div>
+</template>
+
+<script>
+import WorkspaceBilling from '../billing/Workspace';
+
+export default {
+  name: 'AccountBilling',
+  components: {
+    WorkspaceBilling
+  },
+  computed: {
+    workspaces() {
+      return this.$store.state.workspaces.list;
+    }
+  }
+};
+</script>
+
+<style>
+  @import "../../styles/settings-window-page.css";
+  .account-billing {
+    &__label {
+      margin-bottom: 15px;
+    }
+  }
+</style>
