ca2e18fbc5bbad8359ca8459f262495c0c8951e7
Nikita Melnikov <nikmel2803@gmail.com>
2019-08-31 03:17:11 +0300

    billing page fix workspaces cards

--- src/components/account/Billing.vue
+++ src/components/account/Billing.vue
@@ -1,19 +1,20 @@
 <template>
-  <div class="account-billing settings-window-page">
+  <div class="account-billing-settings settings-window-page">
     <div class="settings-window-page__title">
       {{ $t('settings.billing.title') }}
     </div>
-    <label class="label account-billing__label">{{ $t('settings.billing.ownedWorkspaces') }}</label>
+    <label class="label account-billing-settings__label">{{ $t('settings.billing.ownedWorkspaces') }}</label>
     <WorkspaceBilling
       v-for="workspace in workspaces"
       :key="workspace.id"
+      class="account-billing-settings__own-workspace"
       :workspace="workspace"
     />
   </div>
 </template>
 
 <script>
-import WorkspaceBilling from '../billing/Workspace';
+import WorkspaceBilling from '../billing/WorkspaceCard';
 
 export default {
   name: 'AccountBilling',
@@ -30,9 +31,13 @@ export default {
 
 <style>
   @import "../../styles/settings-window-page.css";
-  .account-billing {
+  .account-billing-settings {
     &__label {
       margin-bottom: 15px;
+    }
+
+    &__own-workspace {
+      margin-bottom: 20px;
     }
   }
 </style>
