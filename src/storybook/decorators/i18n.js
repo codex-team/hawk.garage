abe66851853f34288d95587d8956bc3ce0840079
George Berezhnoy <gohabereg@gmail.com>
2020-08-03 04:16:33 +0300

    Include js files in tsconfig for eslint purposes

--- src/storybook/decorators/i18n.js
+++ src/storybook/decorators/i18n.js
@@ -3,31 +3,28 @@ import store from '@/store';
 import i18n, { loadLanguageAsync } from '@/i18n';
 import { SET_LANGUAGE } from '@/store/modules/app/actionTypes';
 
-
 export default (story, context) => ({
   template: `<story/>`,
   props: {
     locale: {
       type: String,
-      default: context.globals.locale
-    }
+      default: context.globals.locale,
+    },
   },
   created() {
     this.$store.watch(
       state => state.app.language,
       newLang => {
-        console.log({newLang})
         loadLanguageAsync(newLang);
       }
     );
   },
   watch: {
     locale: function (val) {
-      console.log({val})
-      this.$store.dispatch(SET_LANGUAGE, val)
-    }
+      this.$store.dispatch(SET_LANGUAGE, val);
+    },
   },
   i18n,
   router,
-  store
+  store,
 });
