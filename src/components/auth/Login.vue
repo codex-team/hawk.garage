98936b592a2c3b3215220f6eeac11b6d4d057a50
ilyamore88 <themorozik@gmail.com>
2020-11-17 12:59:42 +0300

    change username to email in autocomplete

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -38,7 +38,7 @@ export default {
     return {
       fields: [
         {
-          autoComplete: 'username',
+          autoComplete: 'email',
           label: this.$t('authPages.emailAddress'),
           name: 'email',
           value: '',
