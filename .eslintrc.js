9ca179f05e842d628bc2ce2d4f90f407452dcbda
robonetphy <1998patelumang@gmail.com>
2021-04-25 08:34:34 +0530

    update false positive no-shadow enums

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,42 @@
 module.exports = {
-  extends: [
-    'codex/ts',
-    'plugin:vue/recommended',
-    '@vue/typescript',
-  ],
+  extends: ["codex/ts", "plugin:vue/recommended", "@vue/typescript"],
   parserOptions: {
     project: `./tsconfig.json`,
-    parser: '@typescript-eslint/parser',
+    parser: "@typescript-eslint/parser"
   },
   rules: {
     "@typescript-eslint/no-magic-numbers": "off",
-    'no-unused-vars': 0,
-    '@typescript-eslint/no-unused-vars-experimental': [1, {
-      ignoreArgsIfArgsAfterAreUsed: true,
-    }],
-
-    'no-shadow': ['error', {
-      /**
-       * Adjust for vuex
-       */
-      allow: ['state', 'getters'],
-    } ],
+    "no-unused-vars": 0,
+    "@typescript-eslint/no-unused-vars-experimental": [
+      1,
+      {
+        ignoreArgsIfArgsAfterAreUsed: true
+      }
+    ],
+    /**
+     * False positive of no-shadow rule with ENUMs in eslint
+     * https://github.com/typescript-eslint/typescript-eslint/issues/2483
+     *  
+     */ 
+    "no-shadow": "off",
+    "@typescript-eslint/no-shadow": ["error", { allow: ["state", "getters"] }],
     /**
      * We widely use TS types in .js files` jsdoc, so we disable this rule to prevent reporting such cases
      */
-    'jsdoc/no-undefined-types': 0,
+    "jsdoc/no-undefined-types": 0
   },
   overrides: [
     {
-      files: ['*.vue'],
+      files: ["*.vue"],
       rules: {
         /**
          * Vue files should be written on ts so
          *  - the return type is not needed in jsdoc
          *  - params type is not needed in jsdoc
          */
-        'jsdoc/require-returns': 0,
-        'jsdoc/require-param-type': 0
-      },
+        "jsdoc/require-returns": 0,
+        "jsdoc/require-param-type": 0
+      }
     }
   ]
 };
