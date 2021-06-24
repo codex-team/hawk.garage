c0d266d55783841ba019fbe2c5645d24be0d2bc0
ilyamore88 <themorozik@gmail.com>
2021-06-24 11:30:44 +0300

    add docs

--- src/plugins/types.d.ts
+++ src/plugins/types.d.ts
@@ -2,8 +2,21 @@ import { ConfirmationWindowOptions } from '../components/utils/ConfirmationWindo
 
 declare module 'vue/types/vue' {
   interface Vue {
+    /**
+     * Plugin for working with confirmation window
+     * You can open or close confirmation window with it
+     */
     $confirm: {
+      /**
+       * Open confirmation window
+       *
+       * @param options - confirmation window options
+       */
       open: (options?: ConfirmationWindowOptions) => void;
+
+      /**
+       * Close confirmation window
+       */
       close: () => void;
     };
   }
