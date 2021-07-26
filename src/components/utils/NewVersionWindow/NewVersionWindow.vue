df3f34a4b303dcefc9c18ee25b820343ea2e809d
Umang G. Patel <23169768+robonetphy@users.noreply.github.com>
2021-07-26 23:09:39 +0530

    pixel perfected ui added

--- src/components/utils/NewVersionWindow/NewVersionWindow.vue
+++ src/components/utils/NewVersionWindow/NewVersionWindow.vue
@@ -8,24 +8,39 @@
     <div class="new-version-dialog__wrapper">
       <div class="new-version-window__wrapper">
         <div class="new-version-window__title">
-          {{ description }}
-          <UiButton
-            :submit="true"
-            content="Refresh"
-            class="new-version-window__continue-button"
-            @click="
-              () => {
-                onConfirm();
-                close();
-              }
-            "
-          />
-          <UiButton
-            secondary
-            :content="$t('components.confirmationWindow.cancel')"
-            @click="close()"
-          />
+          The new version is available
         </div>
+        <UiButton
+          :submit="true"
+          content="Refresh"
+          class="new-version-window__refresh-button"
+          @click="
+            () => {
+              onRefresh();
+              close();
+            }
+          "
+        />
+        <UiButton
+          secondary
+          content="What’s new? 😏"
+          class="new-version-window__whatsnew-button"
+          @click="
+            () => {
+              onWhatsNew();
+              close();
+            }
+          "
+        />
+        <Icon
+          symbol="close"
+          class="new-version-window__close-button"
+          @click="
+            () => {
+              close();
+            }
+          "
+        />
       </div>
     </div>
   </transition>
@@ -34,14 +49,15 @@
 <script lang="ts">
 import Vue from 'vue';
 import UiButton from '../UiButton.vue';
-
+import Icon from '../Icon.vue';
 /**
  * @link './README.md'
  */
 export default Vue.extend({
   name: 'NewVersionWindow',
   components: {
-    UiButton
+    UiButton,
+    Icon
   },
   data() {
     return {
@@ -49,19 +65,6 @@ export default Vue.extend({
        * Is confirmation window open
        */
       isOpened: false,
-
-      /**
-       * Description of confirming action
-       */
-      description: 'The new version is available',
-
-      /**
-       * onConfirm callback when user clicks continue button
-       */
-      onConfirm: () => {
-        window.location.reload();
-      },
-
     }
   },
   methods: {
@@ -73,13 +76,26 @@ export default Vue.extend({
     open() {
       this.isOpened = true;
     },
-
+    /**
+   * onRefresh callback when user clicks continue button
+   */
+    onRefresh: () => {
+       window.location.reload();
+    },
+    /**
+     * onWhatsNew callback when user clicks continue button
+     */
+    onWhatsNew: () => {
+      window.open('docs', '_blank');
+    },
     /**
      * Hide confirmation window
      */
     close() {
       this.isOpened = false;
     }
+
+
   }
 });
 </script>
@@ -130,7 +146,26 @@ export default Vue.extend({
 
   &__title {
     font-size: 16px;
-    font-weight: bold;
+    font-weight: normal;
+    display: inline;
+    margin: 2px 14px 2px 0;
+  }
+
+  &__refresh-button {
+    padding: 5px 11px;
+    margin-right: 10px;
+    border-radius: 13px;
+  }
+  &__whatsnew-button {
+    padding: 5px 11px;
+    border-radius: 13px;
+    margin-right: 15px;
+  }
+  &__close-button {
+    width: 14px;
+    height: 14px;
+    vertical-align: middle;
+    cursor: pointer;
   }
 }
 </style>
