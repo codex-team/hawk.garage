f71e997431a217a4ed249790249eedb4ff21f951
Ilya Moroz <themorozik@gmail.com>
2021-08-24 14:40:49 +0300

    add revoke button

--- src/components/project/settings/Integrations.vue
+++ src/components/project/settings/Integrations.vue
@@ -10,13 +10,24 @@
       class="projects-integrations-settings-page__token"
       :token="project.token"
     />
+    <div class="projects-integrations-settings-page__revoke-container">
+      {{ $t('projects.settings.integrations.revokeText.0') }}
+      <span
+        class="projects-integrations-settings-page__revoke-button"
+        @click="revokeIntegrationToken()"
+      >
+        {{ $t('projects.settings.integrations.revoke') }}
+      </span>
+      {{ $t('projects.settings.integrations.revokeText.1') }}
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import { Project } from '../../../types/project';
+import {Project} from '../../../types/project';
 import TokenBlock from '../TokenBlock.vue';
+import {ActionType} from "../../utils/ConfirmationWindow/types";
 
 export default Vue.extend({
   name: 'ProjectIntegrationsSettings',
@@ -32,6 +43,21 @@ export default Vue.extend({
       required: true,
     },
   },
+  methods: {
+    /**
+     * Revokes integration token with confirmation
+     */
+    revokeIntegrationToken(): void {
+      this.$confirm.open({
+        title: this.$t('projects.settings.integrations.revokeConfirmation.title') as string,
+        description: this.$t('projects.settings.integrations.revokeConfirmation.description') as string,
+        actionType: ActionType.SUBMIT,
+        onConfirm: () => {
+          console.log('Revoke token');
+        }
+      });
+    }
+  }
 });
 </script>
 
@@ -41,6 +67,21 @@ export default Vue.extend({
   .projects-integrations-settings-page {
     &__token {
       margin-top: 15px;
+    }
+
+    &__revoke-container {
+      color: var(--color-text-second);
+      font-size: 14px;
+      line-height: 20px;
+
+      margin-top: 15px;
+    }
+
+    &__revoke-button {
+      display: inline-block;
+      border-bottom: 1px solid var(--color-text-second);
+
+      cursor: pointer;
     }
   }
 </style>
