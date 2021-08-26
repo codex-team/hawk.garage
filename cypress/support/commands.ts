f3c46bf5e2bb4af3529f5e9584d35f332e05bb51
Georgy Berezhnoy <gohabereg@gmail.com>
2021-08-27 02:07:43 +0300

    Add signup and login e2e tests

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -24,15 +24,123 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-declare namespace Cypress {
-  interface Chainable<Subject = any> {
-    visitHawk(): Chainable<Cypress.AUTWindow>
+import * as seedApi from '@/api/seed';
+
+declare global {
+  namespace Cypress {
+    interface Chainable<Subject = any> {
+      visitHawk(path?: string): Chainable<Cypress.AUTWindow>;
+      createUser(email: string, password: string): Chainable<Cypress.Response<string>>
+      login(email?: string, password?: string): Chainable<void>;
+      register(email: string): Chainable<void>;
+      deleteUser(email: string): Chainable<Cypress.Response<boolean>>;
+      clearStorage(): Chainable<void>;
+    }
   }
 }
 
 /**
  * Visits Hawk on localhost
  */
-Cypress.Commands.add('visitHawk', () => {
-  cy.visit('http://localhost:8080');
-})
+Cypress.Commands.add('visitHawk', (path = '') => {
+  cy.visit(`http://localhost:8080/${path}`);
+});
+
+const API_ENDPOINT = 'http://localhost:4000';
+
+/**
+ * Creates new user using seed resolver
+ *
+ * @param email - user email
+ * @param password - user password
+ */
+Cypress.Commands.add('createUser', (email: string, password: string) => {
+  return cy.request(
+    'POST',
+    API_ENDPOINT + '/graphql',
+    {
+      // language=GraphQL
+      query: `
+        mutation createUser($email: String!, $password: String!) {
+          createUser(email: $email, password: $password) {
+            id
+          }
+        }
+      `,
+      variables: {
+        email,
+        password,
+      },
+    }
+  ).then(response => {
+    console.log(response);
+    return response.body.data.createUser.id;
+  });
+});
+
+/**
+ * Deletes user by email using seed resolver
+ *
+ * @param email - user email
+ */
+Cypress.Commands.add('deleteUser', (email: string) => {
+  return cy.request(
+    'POST',
+    API_ENDPOINT + '/graphql',
+    {
+      // language=GraphQL
+      query: `
+        mutation deleteUser($email: String!) {
+          deleteUser(email: $email)
+        }
+      `,
+      variables: { email },
+    }
+  ).then(response => {
+    return response.body.data.deleteUser;
+  });
+});
+
+/**
+ * Fill data on login page and click the button
+ *
+ * @param [email] - user email
+ * @param [password] - user password
+ */
+Cypress.Commands.add('login', (email?: string, password?: string) => {
+  if (email) {
+    cy.get('input#email')
+      .type(email);
+  }
+
+  if (password) {
+    cy.get('input#password')
+      .type(password);
+  }
+
+  cy.contains('Log in')
+    .click();
+});
+
+/**
+ * Fills data on signup page and click Register button
+ *
+ * @param email - user email
+ */
+Cypress.Commands.add('register', (email: string) => {
+  cy.get('input#email')
+    .type(email);
+
+  cy.contains('Register')
+    .click();
+});
+
+/**
+ * Clears localStorage
+ */
+Cypress.Commands.add('clearStorage', () => {
+  cy.window()
+    .then(win => {
+      win.localStorage.clear();
+    });
+});
