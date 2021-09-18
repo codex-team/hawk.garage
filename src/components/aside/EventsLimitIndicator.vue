2ebdfd281a3a1c2cf9506a2c43ffde63c574c9d8
Umang G. Patel <23169768+robonetphy@users.noreply.github.com>
2021-09-18 08:57:48 +0530

    modify events indicator according to plugin

--- src/components/aside/EventsLimitIndicator.vue
+++ src/components/aside/EventsLimitIndicator.vue
@@ -1,44 +1,32 @@
 <template>
-  <TooltipDialog>
-    <template v-slot:target>
-      <router-link
-        :to="{
-          name: 'workspace-settings-billing',
-          params: { workspaceId: workspace.id },
-        }"
-      >
-        <CircleProgress :current="eventsCount" :max="plan.eventsLimit || 0" />
-      </router-link>
-    </template>
-    <template v-slot:tooltip-content>
-      <div class="events-limit-indicator__info-section">
-        <div class="events-limit-indicator__label">
-          {{ $t("billing.volume") }}
-          <PositiveButton
-            v-if="isEventsLimitExceeded"
-            :content="$t('billing.boost') + '!'"
-          />
-        </div>
-        <div class="events-limit-indicator__info-bar">
-          <div class="events-limi__events">
-            {{ eventsCount || 0 | abbreviateNumber }} /
-            {{ plan.eventsLimit || 0 | abbreviateNumber }}
-            {{ $tc("billing.volumeEvents", eventsCount) }}
-          </div>
-          <Progress
-            :max="plan.eventsLimit || 0"
-            :current="eventsCount"
-            :color="
-              eventsCount / (plan.eventsLimit || eventsCount) >= 0.9
-                ? 'var(--color-indicator-critical)'
-                : 'rgba(219, 230, 255, 0.6)'
-            "
-            class="events-limit-indicator__volume-progress"
-          />
-        </div>
+  <div class="events-limit-indicator__tooltip-dialog">
+    <div class="events-limit-indicator__info-section">
+      <div class="events-limit-indicator__label">
+        {{ $t("billing.volume") }}
+        <PositiveButton
+          v-if="isEventsLimitExceeded"
+          :content="$t('billing.boost') + '!'"
+        />
       </div>
-    </template>
-  </TooltipDialog>
+      <div class="events-limit-indicator__info-bar">
+        <div class="events-limi__events">
+          {{ eventsCount || 0 | abbreviateNumber }} /
+          {{ plan.eventsLimit || 0 | abbreviateNumber }}
+          {{ $tc("billing.volumeEvents", eventsCount) }}
+        </div>
+        <Progress
+          :max="plan.eventsLimit || 0"
+          :current="eventsCount"
+          :color="
+            eventsCount / (plan.eventsLimit || eventsCount) >= 0.9
+              ? 'var(--color-indicator-critical)'
+              : 'rgba(219, 230, 255, 0.6)'
+          "
+          class="events-limit-indicator__volume-progress"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -46,16 +34,12 @@ import Vue from 'vue';
 import PositiveButton from '../utils/PostivieButton.vue';
 import { Plan } from '../../types/plan';
 import Progress from '../utils/Progress.vue';
-import CircleProgress from '../utils/CircleProgress.vue';
-import TooltipDialog from '../utils/TooltipDialog.vue';
 
 export default Vue.extend({
   name: 'EventsLimitIndicator',
   components: {
     PositiveButton,
     Progress,
-    CircleProgress,
-    TooltipDialog,
   },
   props: {
     /**
@@ -96,41 +80,6 @@ export default Vue.extend({
 
 <style>
 .events-limit-indicator {
-  position: relative;
-  margin-left: auto;
-  line-height: 0px;
-
-  &__tooltip-dialog {
-    position: absolute;
-    z-index: 1;
-    top: 140%;
-    right: -170%;
-    background-color: var(--color-bg-second);
-    box-shadow: 0 10px 23px 0 rgba(0, 0, 0, 0.34);
-    border-radius: 10px;
-    opacity: 0;
-    line-height: normal;
-    padding: 15px;
-    transition: opacity 0.1s ease-in;
-    pointer-events: none;
-
-    &::after {
-      position: absolute;
-      bottom: 100%;
-      left: 70%;
-      margin-left: -14px;
-      border-color: transparent transparent var(--color-bg-second) transparent;
-      border-style: solid;
-      border-width: 10px;
-      content: " ";
-    }
-  }
-
-  &:hover &__tooltip-dialog {
-    opacity: 1;
-    pointer-events: auto;
-  }
-
   &__info {
     margin-top: 20px;
 
