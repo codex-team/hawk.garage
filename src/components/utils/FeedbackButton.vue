4ea36a5a8f1cabc8517340ba1718ef129ce39397
Umang G. Patel <23169768+robonetphy@users.noreply.github.com>
2022-04-09 08:13:10 +0530

    feat:feedback button added in utils

---
+++ src/components/utils/FeedbackButton.vue
@@ -0,0 +1,51 @@
+<template>
+  <a href="mailto:team@hawk.so">
+    <div class="feedback-wrapper">
+      <Icon symbol="mail" />
+      <div class="feedback-description">Leave Feedback</div>
+    </div>
+  </a>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Icon from './Icon.vue';
+
+export default Vue.extend({
+  name: 'FeedbackButton',
+  components: {
+    Icon,
+  },
+});
+</script>
+
+<style>
+.feedback {
+  &-wrapper {
+    position: fixed;
+    right: 30px;
+    bottom: 30px;
+    z-index: 9995;
+    background-color: var(--color-indicator-medium);
+    padding: 8px 15px;
+    box-shadow: 0px 9px 16px rgba(16, 106, 189, 0.3);
+    border-radius: 13px;
+    cursor: pointer;
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    svg {
+      height: 23px;
+      width: 28px;
+    }
+  }
+  &-description {
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 30px;
+    margin: 0px 12px;
+  }
+}
+</style>
