c1106ad8efdbd6597dd03286f32b46f2241c9950
Peter Savchenko <specc.dev@gmail.com>
2025-02-04 23:01:39 +0300

    fix(user): improve handling of authentication errors when fetching current user
    - Update fetchCurrentUser API method to use new api.call with error handling
    - Modify user store action to handle unauthenticated errors
    - Reset store if user is not authenticated

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -68,7 +68,7 @@ export async function refreshTokens(refreshToken) {
  * @returns {Promise<User>}
  */
 export async function fetchCurrentUser() {
-  return (await api.callOld(QUERY_CURRENT_USER)).me;
+  return await api.call(QUERY_CURRENT_USER, {}, undefined, { allowErrors: true });
 }
 
 /**
