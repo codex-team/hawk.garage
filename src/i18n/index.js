b6aaeb5653408ea442b907061e1cb999c01dbd8e
Tanya Fomina <fomina.tatianaaa@yandex.ru>
2025-10-08 21:58:10 +0300

    Fix i18n

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,8 +1,14 @@
-import messages from './messages/ru';
+import ruMessages from './messages/ru';
+import enMessages from './messages/en';
 import { createI18n } from 'vue-i18n'
 
 
 export const i18n = createI18n({
   locale: 'ru',
-  messages,
+  fallbackLocale: 'en',
+  messages: {
+    ru: ruMessages,
+    en: enMessages,
+  },
+  legacy: true,
 })
