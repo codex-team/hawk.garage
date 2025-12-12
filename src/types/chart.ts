413e5e9104add954bd073598eb208ee24a428c45
туан <quangtuanitmo18@gmail.com>
2025-12-12 20:07:40 +0300

    refactor(charts): migrate components:chart, chartline  to <script setup>

--- src/types/chart.ts
+++ src/types/chart.ts
@@ -63,3 +63,34 @@ export enum ChartLineColor {
    */
   LightGrey = 'light-grey'
 }
+
+/**
+ * A particular color params
+ */
+export interface ChartLineColors {
+  /**
+   * Name of the color
+   */
+  name: ChartLineColor;
+  /**
+   * Starting color for stroke gradient (top)
+   */
+  strokeStart: string;
+  /**
+   * Ending color for stroke gradient (bottom)
+   */
+  strokeEnd: string;
+  /**
+   * Starting color for fill gradient (top, with opacity)
+   */
+  fillStart: string;
+  /**
+   * Ending color for fill gradient (bottom, usually transparent)
+   */
+  fillEnd: string;
+
+  /**
+   * Pointer + legend color
+   */
+  pointerColor: string;
+}
